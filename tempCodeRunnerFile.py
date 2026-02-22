0099815af42f343c7064029c6c5bf9e7d3a8e4ec
Farimah <farimah.iranshahi@gmail.com>
2026-02-22 10:15:30 +0330

    stop tracking tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-
-    # try:
-    #     output = subprocess.check_output(cmd, env=env).decode()
-    #     return parse_throughput(output)
-    # except subprocess.CalledProcessError as e:
-    #     print("Error executing sdf3:", e)
-    #     print("Output:", e.output.decode() if e.output else "")
-    #     return 0.0
